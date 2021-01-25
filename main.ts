a0ff8cc8ac0a4cd5cfcc7064eefedba9bb5672c7
jasonyaocoder <jason.yao.coder@gmail.com>
2021-01-24 18:16:35 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function moveAmount () {
+    return Speed * Direction2
+}
 function randomImage () {
     ImageIndex = randint(0, 5)
     if (ImageIndex == 0) {
@@ -15,13 +18,12 @@ function randomImage () {
 function goClockPixel () {
     if (1 == ClockpixelChanged) {
         ClockpixelChanged = 0
-    } else if (ClockPixel == SinglePixel) {
-        ClockPixel += Direction2
+    } else if (absDisance(SinglePixel, ClockPixel) < Speed) {
+        ClockPixel += moveAmount()
         ClockPixel = ledCappedIndex(ClockPixel)
         ClockpixelChanged = 1
         Color = randomColor()
         if (ClockPixel % 2 == 0) {
-            randomImage()
             music.playTone(262, music.beat(BeatFraction.Sixteenth))
         } else {
             music.playTone(330, music.beat(BeatFraction.Sixteenth))
@@ -39,10 +41,18 @@ function ledCappedIndex (Index: number) {
 function randomColor () {
     return neopixel.rgb(random255(), random255(), random255())
 }
+input.onButtonPressed(Button.B, function () {
+    Speed += 1
+    Speed = Speed % 3 + 1
+})
+function absDisance (num: number, num2: number) {
+    return Math.abs(100 + num - (100 + num2))
+}
 function random255 () {
     return randint(0, 255)
 }
 let ImageIndex = 0
+let Speed = 0
 let Direction2 = 0
 let Color = 0
 let ClockpixelChanged = 0
@@ -54,8 +64,9 @@ ClockPixel = 0
 ClockpixelChanged = 0
 Color = randomColor()
 Direction2 = 1
+Speed = 1
 basic.forever(function () {
-    SinglePixel += Direction2
+    SinglePixel += moveAmount()
     SinglePixel = ledCappedIndex(SinglePixel)
     goClockPixel()
     Pixel.setPixelColor(SinglePixel, neopixel.colors(NeoPixelColors.Red))
